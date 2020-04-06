b789a4208de7fe7d90c939dc6957597165b118bb
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-04-06 17:17:04 +0100

    Started my Stock-App app!

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Stock App';
+  subHeading = 'Your all in one tikr finder!'
+}
