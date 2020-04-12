64f49daf706b8773063fc2bfd42287ab4631327c
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-04-12 16:30:30 +0100

    Added a table full of legit prices

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -31,7 +32,8 @@ import { TitleComponent } from './shared/title/title.component';
     HighchartsChartModule,
     FormsModule,
     HttpClientModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/search/search.component.ts
+++ src/app/search/search.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { map } from 'rxjs/operators';
 
+import { StockPrice } from '../shared/models/stock-price';
 import { AlphaVantageService } from '../shared/services/alpha-vantage.service';
 
 @Component({
@@ -11,13 +12,13 @@ import { AlphaVantageService } from '../shared/services/alpha-vantage.service';
 })
 export class SearchComponent implements OnInit {
 
-  searchInput: string;
-  searchForm: FormGroup;
-  data;
-  loading = false;
-
   constructor(private alphaVantageSvc: AlphaVantageService) {
   }
+
+  searchInput: string;
+  searchForm: FormGroup;
+  loading = false;
+  stockPrices: StockPrice[];
 
   ngOnInit(): void {
     this.searchForm = new FormGroup({
@@ -26,16 +27,42 @@ export class SearchComponent implements OnInit {
   }
 
   onChangeSearchInput(e) {
-    this.data = null;
     this.loading = true;
     this.searchInput = e.currentTarget.value;
     this.alphaVantageSvc
       .getInstruments(this.searchInput)
       .pipe(
         map(res => {
-          this.data = res;
+          this.stockPrices = this.mapStockPrices(res);
           this.loading = false;
         }))
       .subscribe();
+  }
+
+
+  mapStockPrices(data: any): StockPrice[] {
+
+    const res = new Array<StockPrice>();
+
+    const prices = data['Time Series (Daily)'];
+
+    for (const [key, value] of Object.entries(prices)) {
+      const date = key;
+      const open = value['1. open'];
+      const high = value['2. high'];
+      const low = value['3. low'];
+      const close = value['4. close'];
+      const volume = value['5. volume'];
+
+      res.push({
+        date: new Date(date),
+        open,
+        high,
+        low,
+        close,
+        volume
+      } as StockPrice);
+    }
+    return res;
   }
 }
