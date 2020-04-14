81eb03302709524eb7f66c29cfa1bc0661fc4f2c
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-04-14 16:36:34 +0100

    added some tiles to the top fo my search page

--- src/app/shared/components/company/company.component.ts
+++ src/app/shared/components/company/company.component.ts
@@ -11,13 +11,13 @@ import { AlphaVantageService } from '../../services/alpha-vantage.service';
   styleUrls: ['./company.component.scss']
 })
 export class CompanyComponent implements OnInit {
-  @Output() outputLoadStockPrices: EventEmitter<Company[]> =
+  @Output() outputLoadCompanies: EventEmitter<Company[]> =
     new EventEmitter<Company[]>();
 
   searchInput: string;
   searchForm: FormGroup;
   loading = false;
-  stockPrices: Company[];
+  companies: Company[];
 
   constructor(private alphaVantageSvc: AlphaVantageService) {
   }
@@ -30,14 +30,14 @@ export class CompanyComponent implements OnInit {
 
   onChangeSearchInput(e) {
     this.searchInput = e.currentTarget.value;
-    this.stockPrices = null;
+    this.companies = null;
     this.loading = true;
     this.alphaVantageSvc
       .getCompanies(this.searchInput)
       .pipe(
         map(res => {
-          this.stockPrices = this.mapCompanies(res);
-          this.outputLoadStockPrices.emit(this.stockPrices);
+          this.companies = this.mapCompanies(res);
+          this.outputLoadCompanies.emit(this.companies);
           this.loading = false;
         }))
       .subscribe();
---
+++ src/app/shared/models/company.ts
@@ -0,0 +1,10 @@
+export interface Company {
+    symbol: string;
+    name: string;
+    type: string;
+    region: string;
+    marketOpen: string;
+    marketClose: string;
+    currency: string;
+
+}
