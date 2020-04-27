fc4c558bc36d3a83310b541242894cfa7c8001dc
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-04-27 12:59:04 +0100

    added in firebase config

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -1,11 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-compare',
   templateUrl: './compare.component.html',
   styleUrls: ['./compare.component.scss']
 })
-export class CompareComponent implements OnInit {
+export class CompareComponent {
 
   constructor() { }
 
