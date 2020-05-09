bf098732bc81141e231ffb2a7b25c5c36a9d8741
Benyamin Ahmed <benyaminahmed2@gmail.com>
2020-05-09 14:09:35 +0100

    worked on compare page

--- src/app/compare/compare.component.ts
+++ src/app/compare/compare.component.ts
@@ -1,14 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-compare',
   templateUrl: './compare.component.html',
   styleUrls: ['./compare.component.scss']
 })
-export class CompareComponent {
+export class CompareComponent implements OnInit {
 
-  constructor() { }
+  tickerForm: FormGroup;
+  loading = false;
+  tickerInput: string;
+  tickers: Array<string> = new Array<string>();
 
+  ngOnInit(): void {
 
+    this.tickerForm = new FormGroup({
+      tickerCtrl: new FormControl(this.tickerInput ? this.tickerInput : '')
+    });
 
+  }
+
+  onClickAddTicker(): void {
+    if (this.tickerForm.controls.tickerCtrl.value) {
+      this.tickers.push(this.tickerForm.controls.tickerCtrl.value.toUpperCase());
+    }
+
+  }
 }
